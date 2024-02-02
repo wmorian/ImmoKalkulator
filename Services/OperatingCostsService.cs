5eb4e4838eaaa4fe38077bf655c2cd41bd41f91f
Wahid <wmorian@pm.me>
2024-02-02 23:04:32 +0100

    new Service for calculating operating costs

---
+++ Services/OperatingCostsService.cs
@@ -0,0 +1,40 @@
+using kalkulator.net.Model;
+
+namespace kalkulator.net.Services;
+
+// Bewirtschaftungskosten
+public class OperatingCostsService
+{
+    public OperatingCostsCalcs GetOperatingCostsCalcs(Calculation calculation)
+    {
+        var operatingCosts = calculation.OperatingCosts ?? throw new Exception($"{nameof(calculation.OperatingCosts)} cannot be null!");
+        var rent = calculation.Rent ?? throw new Exception($"{nameof(calculation.Rent)} cannot be null!");
+        var reserves = calculation.Reserves ?? throw new Exception($"{nameof(calculation.Reserves)} cannot be null!");
+
+        var sumOperationCostsAllocable = operatingCosts.HousingAllowanceAllocable 
+            + operatingCosts.PropertyTax 
+            + operatingCosts.OtherCosts.Where(c => c.IsAllocable).Sum(c => c.Cost);
+        
+        var warmRent = rent.TotalColdRent + rent.ParkingSpaces + rent.Other + sumOperationCostsAllocable;
+        var sumOfOperationCostsNonAllocable = operatingCosts.HousingAllowanceNonAllocable 
+            + reserves.MaintenanceReservePerSquareMeterPerAnnum 
+            + (reserves.CalculatedRentLossPercentage / 100 * warmRent)
+            + operatingCosts.OtherCosts.Where(c => !c.IsAllocable).Sum(c => c.Cost);
+
+        return new OperatingCostsCalcs
+        {
+            SumOperationCostsAllocable = sumOperationCostsAllocable,
+            SumOfOperationCostsNonAllocable = sumOfOperationCostsNonAllocable,
+            SumOfHousingAllowance = operatingCosts.HousingAllowanceAllocable + operatingCosts.HousingAllowanceNonAllocable,
+            SumOfOperationCosts = sumOperationCostsAllocable + sumOfOperationCostsNonAllocable
+        };
+    }
+}
+
+public class OperatingCostsCalcs
+{
+    public double SumOperationCostsAllocable { get; set; } // umlagefähige Kosten
+    public double SumOfOperationCostsNonAllocable { get; set; } // nicht umlagefähige Kosten
+    public double SumOfHousingAllowance { get; set; } // Hausgeld, excluding WEG Ruecklage
+    public double SumOfOperationCosts { get; set; }
+}
